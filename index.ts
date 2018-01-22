80c7693783b97800ef898b55ff6bb2c50b5891e2
Zach Bloomquist <zbloomq@live.com>
2018-01-22 11:43:27 -0500

    Project setup

---
+++ index.ts
@@ -0,0 +1,24 @@
+import * as express from 'express'
+import * as morgan from 'morgan'
+
+const app = express()
+
+app.use(morgan('combined')) // Apache log handler
+
+function sendError(res: express.Response, err: string, code?: number, debug?: any) {
+    res.json({
+        'success': false,
+        'error': err,
+        'debug': (process.env.DEBUG ? debug : undefined)
+    }).status(code ? code : 500)
+}
+
+function sendResponse(res: express.Response, value: any) {
+    res.json({
+        'success': true,
+        'result': value
+    }).end()
+}
+
+app.listen(process.env.PORT || 8080,
+    () => console.log('Listening on port ' + (process.env.PORT || 8080)))
